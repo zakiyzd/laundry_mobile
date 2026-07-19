8da4e6de576fb8434024ea9845124deaeca25f07
zakiyzd <zakiyazid10@gmail.com>
2026-07-19 15:37:35 +0700

    build: dynamically force allowInsecureProtocol across all subprojects

--- withAndroidInsecureRepo.js
+++ withAndroidInsecureRepo.js
@@ -3,11 +3,19 @@ const { withProjectBuildGradle } = require('@expo/config-plugins');
 module.exports = function withAndroidInsecureRepo(config) {
   return withProjectBuildGradle(config, (modConfig) => {
     if (modConfig.modResults.contents) {
-      // Memaksa Gradle mengizinkan insecure protocol di semua blok maven repositories
-      modConfig.modResults.contents = modConfig.modResults.contents.replace(
-        /maven\s*\{\s*url\s*['"]http:\/\/[^'"]+['"]\s*\}/g,
-        (match) => match.replace('}', 'allowInsecureProtocol = true \n}')
-      );
+      const insecureRepoRule = `
+        allprojects {
+            repositories {
+                all { repo ->
+                    if (repo.hasProperty('url') && repo.url.toString().startsWith("http://")) {
+                        repo.allowInsecureProtocol = true
+                    }
+                }
+            }
+        }
+      `;
+      
+      modConfig.modResults.contents += `\n${insecureRepoRule}\n`;
     }
     return modConfig;
   });
