ffce470083bf71e296222ba629dfd7fc200d690f
zakiyzd <zakiyazid10@gmail.com>
2026-07-19 16:15:06 +0700

    build: forcefully bump compileSdkVersion to 34 in printer dependency

--- withAndroidInsecureRepo.js
+++ withAndroidInsecureRepo.js
@@ -12,26 +12,32 @@ module.exports = function withAndroidInsecureRepo(config) {
     if (fs.existsSync(printerGradlePath)) {
       let content = fs.readFileSync(printerGradlePath, 'utf8');
       
-      // 1. Ubah SEMUA link HTTP internal menjadi HTTPS (Solusi eror sebelumnya)
+      // 1. Ubah SEMUA link HTTP internal menjadi HTTPS
       if (content.includes('http://')) {
         content = content.replace(/http:\/\//g, 'https://');
       }
 
-      // 2. 🌟 SOLUSI BARU: Ubah perintah jadul 'compile' menjadi 'implementation' agar kompatibel dengan Gradle modern
+      // 2. Ubah perintah jadul 'compile' menjadi 'implementation'
       if (content.includes('compile(') || content.includes('compile ')) {
-        // Mengganti compile fileTree, compile project, atau compile biasa
         content = content.replace(/compile\s*\(/g, 'implementation(');
         content = content.replace(/compile\s+/g, 'implementation ');
-        console.log('[InsecureRepoPlugin] Sukses memperbarui method compile() menjadi implementation()!');
       }
+
+      // 3. 🌟 SOLUSI BARU: Dongkrak SDK Version secara paksa ke Android 34 agar lolos Java 9+ compilation
+      content = content.replace(/compileSdkVersion\s+\d+/g, 'compileSdkVersion 34');
+      content = content.replace(/targetSdkVersion\s+\d+/g, 'targetSdkVersion 34');
       
+      // Jika ada buildToolsVersion jadul, kita hapus atau sesuaikan biar ikut ke root project
+      content = content.replace(/buildToolsVersion\s+['"].*?['"]/g, 'buildToolsVersion "34.0.0"');
+
       fs.writeFileSync(printerGradlePath, content, 'utf8');
+      console.log('[InsecureRepoPlugin] Sukses menaikkan SDK Version library printer ke level 34!');
     }
   } catch (error) {
     console.log('[InsecureRepoPlugin] Gagal menyisir node_modules:', error);
   }
 
-  // Tetap pasang jaring pengaman global di root proyek
+  // Jaring pengaman global di root proyek
   return withProjectBuildGradle(config, (modConfig) => {
     if (modConfig.modResults.contents) {
       const fallbackRule = `
